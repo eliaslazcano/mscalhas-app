6eb5bad2dedb572c02c119139c63b9d78568860e
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-05-18 17:25:42 -0400

    Higienização de layout

--- src/App.vue
+++ src/App.vue
@@ -38,21 +38,13 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
