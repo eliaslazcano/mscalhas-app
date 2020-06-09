dc66985b47573731e204de54ecb8ef21f6b26dcf
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-06-09 17:15:48 -0400

    Tela de serviço aprimorada

--- src/views/Servico.vue
+++ src/views/Servico.vue
@@ -1,55 +1,96 @@
 <template>
   <v-container>
-    <!-- Status e Pagamento -->
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-card :color="finalizado ? 'success' : 'amber'" dark height="9.2rem" class="d-flex flex-column">
-          <v-card-subtitle class="pb-0 text-center"><v-icon class="mr-2">mdi-progress-wrench</v-icon>STATUS DO SERVIÇO</v-card-subtitle>
-          <div class="flex-grow-1 d-flex align-center justify-center flex-column">
-            <v-card-title class="py-0 justify-center">{{finalizado ? 'FINALIZADO' : 'EM ANDAMENTO'}}</v-card-title>
-            <p v-if="finalizado" class="caption mb-0">EM {{corrigeDateTime(servico.data_finalizacao)}}</p>
-          </div>
-          <div class="d-flex justify-center">
-            <v-btn
-              outlined
-              class="mb-2"
-              :disabled="loading"
-              @click="switchFinished"
-            >{{finalizado ? 'Voltar para "em andamento"' : 'Mudar para finalizado'}}</v-btn>
-          </div>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-card color="red" dark height="9.2rem" class="d-flex flex-column">
-          <v-card-subtitle class="pb-0 text-center"><v-icon class="mr-2">mdi-cash-multiple</v-icon>PAGAMENTO</v-card-subtitle>
-          <div class="flex-grow-1 d-flex align-center justify-center">
-            <v-card-title class="py-0 justify-center">NÃO FOI PAGO</v-card-title>
-          </div>
-          <div class="d-flex justify-center">
-            <v-btn
-              outlined
-              class="mb-2"
-              :disabled="loading"
-              @click="dialogPagamentos = true"
-            >Ajustar pagamentos</v-btn>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-select
-      label="Sócio responsável"
-      v-model="servico.socio_responsavel"
-      :items="socios"
-      item-value="id"
-      item-text="nome"
-      no-data-text="Nenhum sócio disponível"
-      placeholder="Clique para escolher o sócio responsável"
-      outlined
-      hide-details
-      class="my-3"
-    ></v-select>
+    <v-card :loading="loading">
+      <v-card-text>
+        <!-- Status e Pagamento -->
+        <v-row v-if="id">
+          <v-col cols="12" sm="6" class="pt-0">
+            <v-card
+              :color="finalizado ? 'success' : 'amber'"
+              dark
+              height="9.2rem"
+              class="d-flex flex-column"
+              :loading="loading"
+            >
+              <v-card-subtitle class="pb-0 text-center"><v-icon class="mr-2">mdi-progress-wrench</v-icon>STATUS DO SERVIÇO</v-card-subtitle>
+              <div class="flex-grow-1 d-flex align-center justify-center flex-column">
+                <v-card-title class="py-0 justify-center">{{finalizado ? 'FINALIZADO' : 'EM ANDAMENTO'}}</v-card-title>
+                <p v-if="finalizado" class="caption mb-0">EM {{corrigeDateTime(servico.data_finalizacao)}}</p>
+              </div>
+              <div class="d-flex justify-center">
+                <v-btn
+                  outlined
+                  class="mb-2"
+                  :disabled="loading"
+                  @click="switchFinished"
+                >{{finalizado ? 'Voltar para "em andamento"' : 'Mudar para finalizado'}}</v-btn>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" class="pt-0">
+            <v-card
+              :color="valorPago === 0 ? 'red' : (valorPago >= servico.valor ? 'success' : 'amber')"
+              dark
+              height="9.2rem"
+              class="d-flex flex-column"
+              :loading="loading"
+            >
+              <v-card-subtitle class="pb-0 text-center"><v-icon class="mr-2">mdi-cash-multiple</v-icon>PAGAMENTO</v-card-subtitle>
+              <div class="flex-grow-1 d-flex align-center justify-center flex-column">
+                <v-card-title class="py-0 justify-center">{{valorPago > 0 ? 'R$ ' + valorPago.toFixed(2).replace('.', ',') : 'NÃO FOI PAGO'}}</v-card-title>
+                <template v-if="valorPago !== 0 && servico.valor !== 0">
+                  <p v-if="valorPago < servico.valor" class="caption mb-0">PAGO PARCIALMENTE (FALTA R$ {{(servico.valor - valorPago).toFixed(2).replace('.', ',')}})</p>
+                  <p v-else-if="valorPago >= servico.valor" class="caption mb-0">QUITADO</p>
+                </template>
+              </div>
+              <div class="d-flex justify-center">
+                <v-btn
+                  outlined
+                  class="mb-2"
+                  :disabled="loading"
+                  @click="dialogPagamentos = true"
+                >Ver/Gerir pagamentos</v-btn>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <div>
+              <p class="mb-1">Sócio responsável</p>
+              <v-select
+                v-model="servico.socio_responsavel"
+                :items="socios"
+                item-value="id"
+                item-text="nome"
+                no-data-text="Nenhum sócio disponível"
+                placeholder="Clique para escolher o sócio responsável"
+                outlined
+                hide-details
+                dense
+              ></v-select>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <div>
+              <p class="mb-1">Valor <span v-if="valorPago === 0">estimado</span> do serviço</p>
+              <v-input hide-details>
+                <money
+                  class="form-control font-weight-bold w-100"
+                  v-model="servico.valor"
+                  v-bind="{prefix: 'R$ ', precision: 2, thousands: '.', decimal: ',', masked: false}"
+                  :disabled="loading"
+                ></money>
+              </v-input>
+            </div>
+          </v-col>
+        </v-row>
+        <!-- Sócio responsável -->
+        <!-- Valor estimado do serviço -->
+      </v-card-text>
+    </v-card>
     <!-- Informações do cliente -->
-    <v-card>
+    <v-card class="my-4" :loading="loading">
       <v-card-title class="grey lighten-3">
         <v-icon class="mr-2">mdi-account-tie</v-icon>
         Informações do cliente
@@ -166,7 +207,7 @@
       </v-card-text>
     </v-card>
     <!-- Informações do serviço -->
-    <v-card class="my-4">
+    <v-card class="my-4" :loading="loading">
       <v-card-title class="grey lighten-3">
         <v-icon class="mr-2">mdi-hammer-screwdriver</v-icon>
         Informações do serviço
@@ -191,21 +232,11 @@
           v-model="servico.observacao"
           :disabled="loading"
         ></v-textarea>
-        <v-divider></v-divider>
-        <p class="mb-1 mt-3">Valor do serviço</p>
-        <v-input hide-details>
-          <money
-            class="form-control font-weight-bold w-100"
-            v-model="servico.valor"
-            v-bind="{prefix: 'R$ ', precision: 2, thousands: '.', decimal: ',', masked: false}"
-            :disabled="loading"
-          ></money>
-        </v-input>
       </v-card-text>
     </v-card>
     <!-- Botões -->
     <div class="d-flex justify-center">
-      <v-btn color="primary" @click="saveData" class="mr-2">
+      <v-btn color="primary" @click="saveData" class="mr-2" :loading="loading">
         <v-icon class="mr-1">mdi-sd</v-icon>GRAVAR {{id ? 'ALTERAÇÕES' : 'SERVIÇO'}}
       </v-btn>
       <v-btn color="primary" outlined to="/servicos">{{id ? 'VOLTAR' : 'CANCELAR'}}</v-btn>
@@ -213,17 +244,17 @@
     <!-- Painel de pagamentos -->
     <dialog-pagamentos v-if="id" v-model="dialogPagamentos" :servico.sync="servico"></dialog-pagamentos>
     <!-- DEBUG -->
-    <div class="pa-1 mb-1 ml-1" style="border: 1px solid black; position: fixed; left: 0; bottom: 0; background-color: rgba(255,255,255,.2); font-size: .6rem; z-index: 999">
-      <pre>ID: {{id === null ? 'null' : id}}</pre>
-      <hr>
-      <p class="caption mb-1">servico</p>
-      <pre>{{servico}}</pre>
-    </div>
+<!--    <div class="pa-1 mb-1 ml-1" style="border: 1px solid black; position: fixed; left: 0; bottom: 0; background-color: rgba(255,255,255,.2); font-size: .6rem; z-index: 999">-->
+<!--      <pre>ID: {{id === null ? 'null' : id}}</pre>-->
+<!--      <hr>-->
+<!--      <p class="caption mb-1">servico</p>-->
+<!--      <pre>{{servico}}</pre>-->
+<!--    </div>-->
   </v-container>
 </template>
 
 <script>
-  //TODO - Tela de pagamentos.
+  //TODO - Desabilitar os botões de pagamento e status se ID for nulo (está criando). Com a mensagem de "Grave o serviço para alterar".
   import {DateHelper} from 'eliaslazcano-helpers'
   import DialogPagamentos from "../components/DialogPagamentos";
   export default {
@@ -239,7 +270,8 @@
       dialogPagamentos: false
     }),
     computed: {
-      finalizado() {return this.servico ? (!!this.servico.data_finalizacao) : null}
+      finalizado() {return this.servico ? (!!this.servico.data_finalizacao) : null},
+      valorPago() {return this.servico && this.servico.pagamentos ? this.servico.pagamentos.reduce((accumulator, currentValue) => accumulator + currentValue.valor, 0) : 0}
     },
     methods: {
       async loadData() {
