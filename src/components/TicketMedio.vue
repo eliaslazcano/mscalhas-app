9d2dd5bf5753e339a721347e2e8cc4bc37e3d3ad
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-06-14 22:58:35 -0400

    Painel de TicketMedio

---
+++ src/components/TicketMedio.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card color="primary">
+    <v-card-text>
+      <p class="body-1 mb-0">Ticket Médio</p>
+      <p class="title">R$ 500,00</p>
+      <p class="caption">Faturamento/Serviço</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "TicketMedio"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Relatorios.vue
+++ src/views/Relatorios.vue
@@ -1,17 +1,22 @@
 <template>
   <v-container>
-    <h1 class="text-center title">Em desenvolvimento...</h1>
-    <ul>
-      <li>Ticket médio (faturamento dividido pelo número de serviços, dia/mes/semana/ano ou então os últimos 100 serviços)</li>
-      <li>Faturamento total (de todos os sócios dia/mes/ano) com indicadores do dia/mes/ano passados</li>
-      <li>Performance dos sócios (quantidade de serviços e respectivo faturamento, por dia/semana/mes/ano e data especifica) [subtela]</li>
-    </ul>
+    <ticket-medio></ticket-medio>
+    <div>
+      <h1 class="text-center title">Em desenvolvimento...</h1>
+      <ul>
+        <li>Ticket médio (faturamento dividido pelo número de serviços, dia/mes/semana/ano ou então os últimos 100 serviços)</li>
+        <li>Faturamento total (de todos os sócios dia/mes/ano) com indicadores do dia/mes/ano passados</li>
+        <li>Performance dos sócios (quantidade de serviços e respectivo faturamento, por dia/semana/mes/ano e data especifica) [subtela]</li>
+      </ul>
+    </div>
   </v-container>
 </template>
 
 <script>
+  import TicketMedio from "../components/TicketMedio";
   export default {
-    name: "Relatorios"
+    name: "Relatorios",
+    components: {TicketMedio}
   }
 </script>
 
